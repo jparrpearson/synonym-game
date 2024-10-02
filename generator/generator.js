09d6ab09e7ba90c98b83dafb8347e7a542269627
Jeremy Parr-Pearson <jparrpearson@users.noreply.github.com>
2024-10-01 21:43:38 -0700

    Change scoring

--- generator/generator.js
+++ generator/generator.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
-const MIN_SYNONYM_COUNT = 3;
-// TODO: Find a better source with fewer obscure words
+// TODO: Find a better source with fewer obscure words (create different generator for each input)
 // See https://www.kaggle.com/datasets/dfydata/wordnet-dictionary-thesaurus-files-in-csv-format
-const INPUT_FILE = './WordnetSynonyms.csv';
-const OUTPUT_FILE = '../js/synonyms.js';
+const INPUT_FILE = (process.env.INPUT_FILE || './WordnetSynonyms.csv');
+const OUTPUT_FILE = (process.env.OUTPUT_FILE || '../js/synonyms.js');
+const MIN_SYNONYM_COUNT = 4;
 
-// Generate 2D array of words to synonyms list
+// Capitalized each word
+function capitalize(str) {
+    if (!str) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Generate 2D array of words to synonyms list (e.g. [[word,[synonym1, synonym2, synonym3]]])
 function generateWordSynonymsArray(csvString) {
     const rows = csvString.trim().split('\n');
     const result = [];
@@ -14,10 +20,12 @@ function generateWordSynonymsArray(csvString) {
     // Expect word, count, pos, synonyms (synonyms are semicolon-delimited and sometimes pipe-delimited)
     for (const row of rows) {
         const [word, count, pos, synonyms] = row.split(',');
+        const capitalizedWord = capitalize(word.trim());
         const synonymList = synonyms.split(/;|\|/).map(s => s.trim());
         const uniqueSynonyms = [...new Set(synonymList)];
+        const capitalizedSynonyms = uniqueSynonyms.map(synonym => capitalize(synonym));
         if (uniqueSynonyms.length > MIN_SYNONYM_COUNT) {
-            result.push([word.trim(), uniqueSynonyms]);
+            result.push([capitalizedWord, capitalizedSynonyms]);
         }
     }
 
